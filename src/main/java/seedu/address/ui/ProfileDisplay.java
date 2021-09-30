76101677c6e760363c769310d0e79e3c5493ae3f
pss-coder <pawandeepse@me.com>
2021-09-30 14:19:02 +0800

    Fix Indentation Checkstyle

--- src/main/java/seedu/address/ui/ProfileDisplay.java
+++ src/main/java/seedu/address/ui/ProfileDisplay.java
@@ -1,10 +1,6 @@
 package seedu.address.ui;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
-
-import static java.util.Objects.requireNonNull;
 
 public class ProfileDisplay extends UiPart<Region> {
 
