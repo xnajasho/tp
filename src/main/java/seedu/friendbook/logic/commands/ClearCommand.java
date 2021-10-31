40042d66c0e97c076d8fa4749a2f900ceae685bd
pss-coder <pawandeepse@me.com>
2021-10-31 16:45:46 +0800

    Create Clear Command Confirmation Prompt

--- src/main/java/seedu/friendbook/logic/commands/ClearCommand.java
+++ src/main/java/seedu/friendbook/logic/commands/ClearCommand.java
@@ -2,6 +2,8 @@ package seedu.friendbook.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import seedu.friendbook.model.FriendBook;
 import seedu.friendbook.model.Model;
 
@@ -13,13 +15,22 @@ public class ClearCommand extends Command {
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Clears friend and birthday list entirely. \n"
             + "\nExample: " + COMMAND_WORD;
-    public static final String MESSAGE_SUCCESS = "Friend book has been cleared!";
+    public static final String MESSAGE_SUCCESS = "Friendbook has been cleared!";
+    public static final String MESSAGE_NO = "Friendbook remains unchanged!";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.setFriendBook(new FriendBook());
-        return new CommandResult(MESSAGE_SUCCESS);
+        Alert clearConfirmationPrompt = new Alert(Alert.AlertType.CONFIRMATION,
+                "are sure you want to lose your friends?", ButtonType.NO, ButtonType.YES);
+        clearConfirmationPrompt.setTitle("confirm clearing Friendbook?");
+        clearConfirmationPrompt.showAndWait();
+        if (clearConfirmationPrompt.getResult() == ButtonType.YES) {
+            model.setFriendBook(new FriendBook());
+            return new CommandResult(MESSAGE_SUCCESS);
+        } else {
+            return new CommandResult(MESSAGE_NO);
+        }
     }
 }
