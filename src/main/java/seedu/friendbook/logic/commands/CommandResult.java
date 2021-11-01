8b527fc4931e61777d52d35de3199bfcdd65bb8c
pss-coder <pawandeepse@me.com>
2021-11-02 00:10:26 +0800

    Fix CommandResult Checkstyle

--- src/main/java/seedu/friendbook/logic/commands/CommandResult.java
+++ src/main/java/seedu/friendbook/logic/commands/CommandResult.java
@@ -19,10 +19,6 @@ public class CommandResult {
 
     /** The application should exit. */
     private final boolean exit;
-
-    /** Prompt for clear command. */
-    private final boolean clearPrompt;
-
     /** Command result to view a specific person. */
     private final Optional<Person> personToView;
 
@@ -33,7 +29,6 @@ public class CommandResult {
         this.feedbackToUser = requireNonNull(feedbackToUser);
         this.showHelp = showHelp;
         this.exit = exit;
-        this.clearPrompt = false;
         this.personToView = Optional.ofNullable(null);
     }
 
@@ -44,22 +39,8 @@ public class CommandResult {
         this.feedbackToUser = requireNonNull(feedbackToUser);
         this.showHelp = showHelp;
         this.exit = exit;
-        this.clearPrompt = false;
         this.personToView = Optional.of(personToView);
     }
-
-    /**
-     * Constructs a {@code CommandResult} with the specified fields to clear prompt.
-     */
-    public CommandResult(String feedbackToUser, boolean showHelp, boolean exit,
-                         boolean clearPrompt) {
-        this.feedbackToUser = requireNonNull(feedbackToUser);
-        this.showHelp = showHelp;
-        this.exit = exit;
-        this.clearPrompt = clearPrompt;
-        this.personToView = Optional.of(null);
-    }
-
     /**
      * Constructs a {@code CommandResult} with the specified {@code feedbackToUser},
      * and other fields set to their default value.
@@ -67,16 +48,6 @@ public class CommandResult {
     public CommandResult(String feedbackToUser) {
         this(feedbackToUser, false, false);
     }
-
-    /**
-     * Constructs a {@code CommandResult} with the specified {@code feedbackToUser},
-     * and {@code clearPrompt} and other fields set to their default value.
-     */
-    public CommandResult(String feedbackToUser, boolean clearPrompt) {
-        this(feedbackToUser, false, false, clearPrompt);
-    }
-
-
     public String getFeedbackToUser() {
         return feedbackToUser;
     }
@@ -96,11 +67,6 @@ public class CommandResult {
     public boolean isExit() {
         return exit;
     }
-
-    public boolean isClearPrompt() {
-        return clearPrompt;
-    }
-
     @Override
     public boolean equals(Object other) {
         if (other == this) {
