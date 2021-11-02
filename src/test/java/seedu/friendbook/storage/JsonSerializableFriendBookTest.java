c52799c35c26ec92fc81aa708dc66d9aba87a88e
SajalVaishnav <76965917+SajalVaishnav@users.noreply.github.com>
2021-11-02 20:47:08 +0800

    test bug

--- src/test/java/seedu/friendbook/storage/JsonSerializableFriendBookTest.java
+++ src/test/java/seedu/friendbook/storage/JsonSerializableFriendBookTest.java
@@ -14,7 +14,7 @@ public class JsonSerializableFriendBookTest {
 
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableAddressBookTest");
     private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalPersonsFriendBook.json");
-    private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidNameInPersonFriendBook.json");
+    private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPersonFriendBook.json");
     private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonFriendBook.json");
 
     /*@Test
